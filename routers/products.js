cedbe45c06f233a9a521a52cfed5bcff264035ec
Orxan-Ibrahimov <orxan.ibrahimli.98@gmail.com>
2022-03-20 17:36:49 +0400

    AddedSingleImageUploadToApi

--- routers/products.js
+++ routers/products.js
@@ -3,15 +3,46 @@ const express = require('express')
 const { Category } = require('../models/category')
 const router = express.Router()
 const mongoose = express('mongoose')
+const multer = require('multer')
 
-router.post('/', async (req, res) => {
+const FileTypes = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg'    
+}
+
+var storage = multer.diskStorage({
+    destination: function(req,file,cb) {
+        const isValid = FileTypes[file.mimetype];
+        let typeError = new Error("invalid Image Type");
+        if(isValid) typeError = null;
+        cb(typeError,"public/uploads")
+    },
+    filename: function(req,file,cb) {
+        const filename = file.originalname.split(" ").join("-");
+        const extension = FileTypes[file.mimetype];
+        cb(null, `${filename}${Date.now()}.${extension}`)
+        // cb(null, `${filename}${Date.now()}`)
+    }
+});
+
+const uploadsOption = multer({storage: storage});
+
+router.post('/',uploadsOption.single('image'),async (req, res) => {
     const category = await Category.findById(req.body.category)
 
     if (!category) return res.status(400).send('Category is invalid')
 
+    const file = req.file;
+    if (!file) return res.status(400).send('Image is not in the request!')
+    
+    const filename = req.file.filename;
+    const baseUrl = `${req.protocol}://${req.get('host')}/public/uploads/`;
+
+
     let product = new Product({
         name: req.body.name,
-        image: req.body.image,
+        image: `${baseUrl}${filename}`,
         brand: req.body.brand,
         price: req.body.price,
         rating: req.body.rating,
